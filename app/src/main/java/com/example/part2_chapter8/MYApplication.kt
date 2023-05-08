0cae54b11cb6f45204fe360961433ccddbeb8a47
david-s-kim <mycodingtool9@gmail.com>
2023-05-08 23:50:32 +0900

    지도 앱

---
+++ app/src/main/java/com/example/part2_chapter8/MYApplication.kt
@@ -0,0 +1,18 @@
+package com.example.part2_chapter8
+
+import android.app.Application
+import android.content.Context
+
+class MYApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MYApplication.applicationContext = applicationContext
+    }
+
+    companion object {
+        lateinit var applicationContext: Context
+    }
+
+
+}
